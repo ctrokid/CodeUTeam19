b3ff01d18d177fb5c5c38f52ac2efd37ca929d0e
Collin Trowbridge <ctrokid@gmail.com>
2019-02-10 09:15:24 -0800

    Add simple MessageTest.java and junit dependency.

---
+++ src/test/java/com/google/codeu/data/MessageTest.java
@@ -0,0 +1,21 @@
+package com.google.codeu.data;
+
+import java.time.Instant;
+import java.util.UUID;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MessageTest {
+
+  @Test
+  public void testCreateMessage() {
+    String user = "test_user";
+    String text = "test text";
+
+    Message message = new Message(user, text);
+
+    Assert.assertNotEquals(0, message.getId());
+    Assert.assertEquals(text, message.getText());
+    Assert.assertEquals(user, message.getUser());
+  }
+}
