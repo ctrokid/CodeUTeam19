793f925d3d425b472bdd63093f983ccb9fed96f4
sauloferreira <srf6413@g.rit.edu>
2019-02-23 18:28:03 -0500

    added AboutMeServlet

--- src/main/java/com/google/codeu/servlets/AboutMeServlet.java
+++ src/main/java/com/google/codeu/servlets/AboutMeServlet.java
@@ -10,8 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Responds with a hard-coded message for testing purposes.
  */
-@WebServlet("/stats")
-public class StatsPageServlet extends HttpServlet{
+@WebServlet("/about")
+public class AboutMeServlet extends HttpServlet{
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
