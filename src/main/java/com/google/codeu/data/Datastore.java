6c15c3daa758a06b736647f40719f92d0011d877
Saulo Ferreira <35975183+srf6413@users.noreply.github.com>
2019-03-03 19:38:03 -0500

    Removed blank lines

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -103,7 +103,6 @@ public class Datastore {
   /** 
    *Stores the User in Datastore.
    */
-  
   public void storeUser(User user) {
     
     Entity userEntity = new Entity("User", user.getEmail());
@@ -117,7 +116,6 @@ public class Datastore {
    * null if no matching User was found.
    */
   public User getUser(String email) {
-
     Query query = new Query("User")
             .setFilter(new Query.FilterPredicate("email", FilterOperator.EQUAL, email));
     PreparedQuery results = datastore.prepare(query);
