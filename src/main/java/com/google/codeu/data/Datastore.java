68eb43b08f1c3461451a3900dc0331f94c212f80
Saulo Ferreira <35975183+srf6413@users.noreply.github.com>
2019-03-03 19:55:37 -0500

    One more line of whitespace

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -104,7 +104,6 @@ public class Datastore {
    *Stores the User in Datastore.
    */
   public void storeUser(User user) {
-    
     Entity userEntity = new Entity("User", user.getEmail());
     userEntity.setProperty("email", user.getEmail());
     userEntity.setProperty("aboutMe", user.getAboutMe());
