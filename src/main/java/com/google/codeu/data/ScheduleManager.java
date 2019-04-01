57255ff693531d19bd9ddd95df43b0e64bf3fa03
sauloferreira <srf6413@g.rit.edu>
2019-04-01 01:10:20 -0400

    Added ScheduleManager

---
+++ src/main/java/com/google/codeu/data/ScheduleManager.java
@@ -0,0 +1,15 @@
+package com.google.codeu.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScheduleManager {
+  private List<ScheduleItem> schedule;
+
+  /**
+   * Create a new ScheduleManager.
+   */
+  public ScheduleManager() {
+    this.schedule = new ArrayList<>();
+  }
+}
