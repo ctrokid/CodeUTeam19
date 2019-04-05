a0d6bc7abad3bfaebda4f9242c3b5c7d0f486e79
mcardosog <mcardosog@icloud.com>
2019-04-05 12:34:58 -0400

    Day Enum
    Days Enum created. Changed List<String> daysOfWeek to List<Days> daysOfWeek on Course.java

---
+++ src/main/java/com/google/codeu/data/Days.java
@@ -0,0 +1,15 @@
+package com.google.codeu.data;
+
+public enum Days {
+  Monday("Monday"), Tuesday("Tuesday"), Wednesday("Wednesday"),
+  Thursday("Thursday"), Friday("Friday"), Saturday("Saturday"), Sunday("Sunday");
+  private String value;
+
+  Days(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return value;
+  }
+}
