97cc0f12ac9cc84b5823d06a1ee0bbe6fbf8f477
Andrea Yang <yy545@cornell.edu>
2019-04-12 21:30:29 -0400

    OAuth WIP

---
+++ src/main/java/com/google/codeu/servlets/CalendarOAuthServlet.java
@@ -0,0 +1,32 @@
+package com.google.codeu.servlets;
+
+import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
+import com.google.api.client.extensions.appengine.auth.oauth2.AbstractAppEngineAuthorizationCodeServlet;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class CalendarOAuthServlet extends AbstractAppEngineAuthorizationCodeServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws IOException {
+  }
+
+  @Override
+  protected String getRedirectUri(HttpServletRequest req) throws ServletException, IOException {
+    return Utils.getRedirectUri(req);
+  }
+
+  @Override
+  protected AuthorizationCodeFlow initializeFlow() throws IOException {
+    return Utils.newFlow();
+  }
+}
